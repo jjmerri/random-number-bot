313ceb218543a548c38c1d782a190fc2307933e0
jjmerri <jjmerri88@gmail.com>
2018-06-14 18:07:36 -0400

    cosmetic email update

--- random_number_bot_monitor.sh
+++ random_number_bot_monitor.sh
@@ -16,7 +16,7 @@ kill_ret=$?
 if [ "$kill_ret" -ne "0" ] && [ ! -f $mail_sent_file ]
 then
     echo "mail sent" > $mail_sent_file
-    (echo "random_number_bot LOG"; tail -40 $log_file;) | mail -t jjmerri88@gmail.com -s "Random Number Bot Not Running!"
+    (echo "random_number_bot LOG"; tail -40 $log_file;) | mail -t BlobForge@gmail.com -s "Random Number Bot Not Running!"
 fi
 
 if [ "$kill_ret" -eq "0" ] && [ -f $mail_sent_file ]
